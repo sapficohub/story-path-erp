21e0ae7852cd5d35d1beb86f075f1b8399664267
bharadwaz221 <89135377+bharadwaz221@users.noreply.github.com>
2026-07-14 12:55:14 +0530

    Simplify RootShell component HTML structure
    Refactored RootShell component to simplify HTML structure by removing unnecessary script tags and noscript tags.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -107,41 +107,16 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 
 function RootShell({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-  <HeadContent />
+   <html lang="en">
+  <head>
+    <HeadContent />
+  </head>
 
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-        f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-M8PD2672');
-      `,
-    }}
-  />
-</head>
-      <body>
-  <noscript>
-    <iframe
-      src="https://www.googletagmanager.com/ns.html?id=GTM-M8PD2672"
-      height="0"
-      width="0"
-      style={{
-        display: "none",
-        visibility: "hidden",
-      }}
-    />
-  </noscript>
-
-  {children}
-
-  <Scripts />
-</body>
-    </html>
+  <body>
+    {children}
+    <Scripts />
+  </body>
+</html>
   );
 }
 
